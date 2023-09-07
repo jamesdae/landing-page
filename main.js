b37396e1477bf7cd5ecfe3798ebd5b31ee012883
James Yang <jamesdaeyang@gmail.com>
2023-09-07 00:16:12 +0000

    Add error handling for AJAX request. Move script tag in HTML.

--- main.js
+++ main.js
@@ -10,13 +10,19 @@ $(document).ready(() => {
       type: 'GET',
       dataType: 'json'
     }).done((data, status, xhr) => {
-      console.log(data);
       const filtered = data.filter(episode => episode.show.image !== null);
       const tomorrowSchedule = filtered.slice(0, 24);
       $.each(tomorrowSchedule, (index, episode) => {
         const eachImage = $('<img></img>').attr({src: episode.show.image.medium, alt: `Poster for ${episode.show.name}`, title: `${episode.show.name}: ${episode.name}`}).addClass('responsive-image');
         $('.schedule-bar').append(eachImage);
       });
+    }).fail((xhr, status, error) => {
+      console.error("Error: ", status, error);
+      if (xhr.status === 500) {
+        console.error("Server error: Something went wrong on the server.")
+      } else if (xhr.status === 404) {
+        console.error("Error: Resource not found.")
+      }
     })
   
     $('.schedule-bar').on('scroll', (event) => {
